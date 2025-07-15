5c5134bfe60522df43f1fe007b0f680198ee7225
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-15 06:19:36 -0700

    feat(player): add movement in four directions

---
+++ scripts/player.gd
@@ -0,0 +1,17 @@
+class_name Player
+extends CharacterBody2D
+
+func _process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+		
+	velocity = direction * 70
+	move_and_slide()
