44f7efcac4057d956677f9fb8cf5bd27954a8772
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-23 22:38:05 -0700

    feat(enemy): add collateral damage emitter for wall and enemy collision

--- scripts/enemy/girl_enemy.gd
+++ scripts/enemy/girl_enemy.gd
@@ -11,6 +11,7 @@ signal enemy_hurt_emitter
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var damage_receiver: EnemyDamageReceiver = $EnemyDamageReceiver
 @onready var state_machine := $StateMachine
+@onready var collateral_damage_emitter: Area2D = $CollateralDamageEmitter
 
 @export var player: PlayerCharacter
 @export var damage: int
@@ -21,25 +22,31 @@ signal enemy_hurt_emitter
 @export var max_slot_distance: float = 50.0
 # Interval to check for a closer slot (in seconds)
 @export var slot_check_interval: float = 0.5
+@export var flight_speed: float
 
-enum State {IDLE, WALK, HURT1, HURT2, WAKEUP}
+enum State {IDLE, WALK, HURT1, HURT2, WAKEUP, FLY, FALL}
 var states := {
 	State.IDLE: "Idle",
 	State.WALK: "Walk",
 	State.HURT1: "Hurt1",
 	State.HURT2: "Hurt2",
-	State.WAKEUP: "Wakeup"
+	State.WAKEUP: "Wakeup",
+	State.FLY: "Fly",
+	State.FALL: "Fall",
 }
 var current_health := 0
 var enemy_sensor_position: Vector2
 var enemy_damage_sensor: Vector2
 var player_slot: EnemySlot = null
 var slot_check_timer: float = 0.0
+var hittype: EnemyDamageReceiver.HitType
 
 func _ready() -> void:
 	damage_receiver.enemy_damage_receiver.connect(on_receive_damage)
 	enemy_sensor_position = enemy_collision_shape.position
 	enemy_damage_sensor = enemy_damage_receiver.position
+	collateral_damage_emitter.area_entered.connect(on_emit_collateral_damage)
+	collateral_damage_emitter.body_entered.connect(on_wall_hit)
 	current_health = max_health
 	# Attempt initial slot reservation
 	if player != null and player_slot == null:
@@ -97,6 +104,13 @@ func get_sprite_position(_direction: Vector2) -> void:
 		enemy_collision_shape.position.x = -enemy_sensor_position.x
 		enemy_damage_receiver.position.x = -enemy_damage_sensor.x
 
+func on_emit_collateral_damage(receiver: EnemyDamageReceiver) -> void:
+	var direction := Vector2.LEFT if receiver.global_position.x < global_position.x else Vector2.RIGHT
+	receiver.enemy_damage_receiver.emit(0, direction, EnemyDamageReceiver.HitType.KNOCKDOWN)
+
+func on_wall_hit(_wall: AnimatableBody2D) -> void:
+	enemy_hurt_emitter.emit(states[State.FALL])
+
 func on_receive_damage(_damage: int, _direction: Vector2, hit_type: EnemyDamageReceiver.HitType) -> void:
 	current_health = clamp(current_health - _damage, 0, max_health)
 	
@@ -106,6 +120,15 @@ func on_receive_damage(_damage: int, _direction: Vector2, hit_type: EnemyDamageR
 		player_slot = null
 	
 	# Emit HURT2 for death or KNOCKDOWN, otherwise HURT1
-	var state_to_emit = states[State.HURT2] if current_health == 0 or hit_type == EnemyDamageReceiver.HitType.KNOCKDOWN else states[State.HURT1]
+	#var state_to_emit = states[State.HURT2] if current_health == 0 or hit_type == EnemyDamageReceiver.HitType.KNOCKDOWN else states[State.HURT1]
+	var state_to_emit
+	if current_health == 0 or hit_type == EnemyDamageReceiver.HitType.KNOCKDOWN:
+		state_to_emit = states[State.HURT2]
+	elif hit_type == EnemyDamageReceiver.HitType.POWER:
+		state_to_emit = states[State.HURT1]
+		hittype = EnemyDamageReceiver.HitType.POWER
+	else:
+		state_to_emit = states[State.HURT1]
+		hittype = EnemyDamageReceiver.HitType.NORMAL
 	enemy_hurt_emitter.emit(state_to_emit)
 		
