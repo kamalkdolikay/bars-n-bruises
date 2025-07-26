e02dc436927d95824b2aa4c6663813084d7ad81c
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-25 17:55:45 -0700

    feat(enemy): add two attack states

---
+++ scripts/enemy/attack_3_state.gd
@@ -0,0 +1,20 @@
+class_name EnemyAttack3
+extends CharacterState
+
+@export var enemy: EnemyCharacter
+@onready var damage_emitter: Area2D = $"../../DamageEmitter"
+
+func enter() -> void:
+	enemy.play_animation((enemy.states[enemy.State.ATTACK3]).to_lower())
+	damage_emitter.monitoring = true
+	
+func update(_delta: float) -> void:
+	pass
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == (enemy.states[enemy.State.ATTACK3]).to_lower():
+		transition.emit(enemy.states[enemy.State.IDLE])
+
+func exit() -> void:
+	enemy.stop_animation()
+	damage_emitter.monitoring = false
