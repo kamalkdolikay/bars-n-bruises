0c6f5cbb123b78db70e51d0ae7de347974d0cb74
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-27 19:32:13 -0700

    feat(enemy): add spawn enemy and death enemy signals

--- src/core/systems/entity_manager.gd
+++ src/core/systems/entity_manager.gd
@@ -2,3 +2,7 @@ extends Node
 
 @warning_ignore("unused_signal")
 signal spawn_collecible(collectible_global_position: Vector2, collectible_type: Collectible.Type)
+@warning_ignore("unused_signal")
+signal spawn_enemy(enemy_data: EnemyData)
+@warning_ignore("unused_signal")
+signal death_enemy(enemy: BaseEnemy)
