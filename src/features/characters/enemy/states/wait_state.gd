851a5340e15e03c0bb29c897c9f0e7df2187728e
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-29 14:37:56 -0700

    update(enemy): enemy gradually fades in while door open

--- src/features/characters/enemy/states/wait_state.gd
+++ src/features/characters/enemy/states/wait_state.gd
@@ -3,20 +3,32 @@ extends CharacterState
 
 @export var enemy: BaseEnemy
 
+const DURATION_APPEAR := 2000  # milliseconds
 var initial_z_index: int
+var time_start_appearing: int = 0
 
 func enter() -> void:
-	enemy.play_animation("idle")
+	enemy.play_animation((enemy.states[enemy.State.IDLE]).to_lower())
 	initial_z_index = enemy.door.sprite.z_index
+	enemy.modulate.a = 0.0
+	time_start_appearing = Time.get_ticks_msec()
 	
-func update(_delta: float) -> void:
 	if enemy.door.state == Door.State.CLOSED:
-		enemy.door.sprite.z_index = 1
+		enemy.door.sprite.z_index = 1  # bring door in front
 		enemy.door.open()
-	if enemy.door.state == Door.State.OPENED:
-		enemy.door.sprite.z_index = initial_z_index
-		await get_tree().create_timer(0.3).timeout
-		transition.emit("Idle")
+
+func update(_delta: float) -> void:
+	var door_state := enemy.door.state
+
+	if door_state == Door.State.OPENING or door_state == Door.State.OPENED:
+		var time_elapsed := Time.get_ticks_msec() - time_start_appearing
+		var progress = clamp(float(time_elapsed) / DURATION_APPEAR, 0.0, 1.0)
+		enemy.modulate.a = progress
+
+		if door_state == Door.State.OPENED and progress >= 1.0:
+			enemy.door.sprite.z_index = initial_z_index
+			await get_tree().create_timer(0.3).timeout
+			transition.emit(enemy.states[enemy.State.IDLE])
 
 func exit() -> void:
 	enemy.stop_animation()
