e9470baf1912d9e96bda072c9cd2c574f782c907
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-29 01:19:27 -0700

    update(enemy): add enemy drop height and ground position

--- src/core/systems/enemy_data.gd
+++ src/core/systems/enemy_data.gd
@@ -1,9 +1,16 @@
 class_name EnemyData
 extends Resource
 
+const DROP_HEIGHT: int = 260
+
 @export var type: BaseCharacter.Type
 @export var global_position: Vector2
+@export var height: int
 
 func _init(character_type: BaseCharacter.Type = BaseCharacter.Type.GIRL, position: Vector2 = Vector2.ZERO) -> void:
 	type = character_type
-	global_position = position
+	if position.y < 0:
+		height = DROP_HEIGHT
+		global_position = position + Vector2.DOWN * DROP_HEIGHT
+	else:
+		global_position = position
