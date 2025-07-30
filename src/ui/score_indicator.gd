cd972da68b48cfb3647b61d698c953fc68edeefc
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-30 00:18:42 -0700

    feat(ui): score displayed logic

---
+++ src/ui/score_indicator.gd
@@ -0,0 +1,31 @@
+class_name ScoreIndicator
+extends Label
+
+@export var duration_score_update: float
+
+var displayed_score: int = 0
+var prior_score: int = 0
+var real_score: int = 0
+var time_start_update := Time.get_ticks_msec()
+
+func _ready() -> void:
+	displayed_score = 0
+	refresh()
+
+func _process(_delta: float) -> void:
+	if real_score != displayed_score:
+		var progres := (Time.get_ticks_msec() - time_start_update) / duration_score_update
+		if progres < 1:
+			displayed_score = lerp(prior_score, real_score, progres)
+		else:
+			displayed_score = real_score
+		refresh()
+
+func add_combo(points: int) -> void:
+	real_score += int((points * (points + 1)) / 2.0)
+	prior_score = displayed_score
+	time_start_update = Time.get_ticks_msec()
+	refresh()
+
+func refresh() -> void:
+	text = str(displayed_score)
