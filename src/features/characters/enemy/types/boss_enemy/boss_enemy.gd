719d2957cbebe2149240e44abb72fd37cd2a3369
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-29 22:38:31 -0700

    feat(enemy): send damage to healthbar

--- src/features/characters/enemy/types/boss_enemy/boss_enemy.gd
+++ src/features/characters/enemy/types/boss_enemy/boss_enemy.gd
@@ -1,6 +1,37 @@
 class_name BossEnemy
 extends BaseEnemy
 
+func _ready() -> void:
+	super._ready()
+	set_health(max_health, is_hit_once)
+
 # Attack Logic
 func is_player_within_range() -> bool:
 	return (player_slot.global_position - global_position).length() < 25
+
+# Damage Logic
+func _on_receive_damage(damage_amount: int, _direction: Vector2, _hit_type: DamageReceiver.HitType) -> void:
+	set_health(current_health - damage_amount, true)
+	
+	# Free slot only on death
+	if is_dead() and is_instance_valid(player_slot):
+		player.free_slot(self)
+		player_slot = null
+	
+	# Emit HURT2 for death or KNOCKDOWN, otherwise HURT1
+	var state_to_emit
+	if is_dead() or _hit_type == DamageReceiver.HitType.KNOCKDOWN:
+		state_to_emit = states[State.HURT2]
+	elif _hit_type == DamageReceiver.HitType.POWER:
+		state_to_emit = states[State.HURT1]
+		hit_type = DamageReceiver.HitType.POWER
+	else:
+		state_to_emit = states[State.HURT1]
+		hit_type = DamageReceiver.HitType.NORMAL
+	state_machine.on_state_transition(state_to_emit)
+
+# UI
+func set_health(health: int, is_hit: bool) -> void:
+	current_health = clamp(health, 0, max_health)
+	if is_hit:
+		DamageManager.health_change.emit(Type.BOSS, current_health, max_health)
--- src/features/characters/enemy/types/girl_enemy/girl_enemy.gd
+++ src/features/characters/enemy/types/girl_enemy/girl_enemy.gd
@@ -1,2 +1,33 @@
 class_name GirlEnemy
 extends BaseEnemy
+
+func _ready() -> void:
+	super._ready()
+	set_health(max_health, is_hit_once)
+
+# Damage Logic
+func _on_receive_damage(damage_amount: int, _direction: Vector2, _hit_type: DamageReceiver.HitType) -> void:
+	set_health(current_health - damage_amount, true)
+	
+	# Free slot only on death
+	if is_dead() and is_instance_valid(player_slot):
+		player.free_slot(self)
+		player_slot = null
+	
+	# Emit HURT2 for death or KNOCKDOWN, otherwise HURT1
+	var state_to_emit
+	if is_dead() or _hit_type == DamageReceiver.HitType.KNOCKDOWN:
+		state_to_emit = states[State.HURT2]
+	elif _hit_type == DamageReceiver.HitType.POWER:
+		state_to_emit = states[State.HURT1]
+		hit_type = DamageReceiver.HitType.POWER
+	else:
+		state_to_emit = states[State.HURT1]
+		hit_type = DamageReceiver.HitType.NORMAL
+	state_machine.on_state_transition(state_to_emit)
+
+# UI
+func set_health(health: int, is_hit: bool) -> void:
+	current_health = clamp(health, 0, max_health)
+	if is_hit:
+		DamageManager.health_change.emit(Type.GIRL, current_health, max_health)
