b662760ecf376148109af778fb1eb4231c8aad24
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-30 18:22:36 -0700

    feat(ui): flicker tecxture 3 times on screen

---
+++ src/utils/flickering_texture/flickering_texture_rect.gd
@@ -0,0 +1,33 @@
+class_name FlickeringTextureRect
+extends TextureRect
+
+@export var duration_flicker: int
+@export var total_flickers: int
+
+var flickers_left: int = 0
+var image: Texture2D = null
+var is_flickering: bool = false
+var time_start_last_flicker := Time.get_ticks_msec()
+
+func _ready() -> void:
+	image = texture
+	texture = null
+
+func _process(_delta: float) -> void:
+	if is_flickering and (Time.get_ticks_msec() - time_start_last_flicker > duration_flicker):
+		if texture == null:
+			if flickers_left == 0:
+				is_flickering = false
+			else:
+				flickers_left -= 1
+				texture = image
+		else:
+			texture = null
+		
+		time_start_last_flicker = Time.get_ticks_msec()
+
+func start_flickering() -> void:
+	flickers_left = total_flickers
+	is_flickering = true
+	time_start_last_flicker = Time.get_ticks_msec()
+	SoundPlayer.play(SoundManager.Sound.GOGOGO)
