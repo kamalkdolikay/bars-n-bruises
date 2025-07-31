f9827117a497dfda01ca0a22895597aadca1e0b0
Kamal Dolikay <kamaldolikay@gmail.com>
2025-07-30 18:37:31 -0700

    update(ui): signal for camera shake

--- src/core/systems/damage_manager.gd
+++ src/core/systems/damage_manager.gd
@@ -2,3 +2,5 @@ extends Node
 
 @warning_ignore("unused_signal")
 signal health_change(character_type: BaseCharacter.Type, current_health: int, max_health: int)
+@warning_ignore("unused_signal")
+signal heavy_blow_received()
