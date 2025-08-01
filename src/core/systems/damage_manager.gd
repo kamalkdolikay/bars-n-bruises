981d690ff57eb4c9bf1f506a12de71ce33c5c485
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-01 08:06:04 -0700

    feat(player): add signal player revive

--- src/core/systems/damage_manager.gd
+++ src/core/systems/damage_manager.gd
@@ -4,3 +4,5 @@ extends Node
 signal health_change(character_type: BaseCharacter.Type, current_health: int, max_health: int)
 @warning_ignore("unused_signal")
 signal heavy_blow_received()
+@warning_ignore("unused_signal")
+signal player_revive()
