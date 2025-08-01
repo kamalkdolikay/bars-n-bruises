dc5cfb5c20bf1a78df39fd49b0762b05ef415fe6
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-01 15:40:21 -0700

    update(ui): add stage complete signal

--- src/core/systems/stage_manager.gd
+++ src/core/systems/stage_manager.gd
@@ -3,4 +3,6 @@ extends Node
 @warning_ignore("unused_signal")
 signal checkpoint_start
 @warning_ignore("unused_signal")
-signal checkpoint_complete
+signal checkpoint_complete(checkpoint: Checkpoint)
+@warning_ignore("unused_signal")
+signal stage_complete
